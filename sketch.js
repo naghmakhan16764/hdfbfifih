1f0b06512cc9e118d744f84a8e4bbbb23ef61813
naghmakhan16764 <74590340+naghmakhan16764@users.noreply.github.com>
2021-01-21 16:19:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var canvas;
+var music;
+
+function preload(){
+    music = loadSound("music.mp3");
+}
+
+
+function setup(){
+    canvas = createCanvas(800,600);
+createSprite(random(20,750))
+    //create 4 different surfaces
+surface1=CreateSprite(400,200,20,20)
+surface1.shapeColor="green"
+
+surface2=CreateSprite()
+
+
+    //create box sprite and give velocity
+
+}
+
+function draw() {
+    background(rgb(169,169,169));
+    drawSprites()
+}
